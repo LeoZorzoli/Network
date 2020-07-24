cffdb5c1ab3e2634d9a252867d1474cb45d23715
Leonel Zorzoli <63476706+LeoZorzoli@users.noreply.github.com>
2020-07-24 16:26:18 -0300

    Add files via upload

--- project4/settings.py
+++ project4/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -122,3 +124,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
